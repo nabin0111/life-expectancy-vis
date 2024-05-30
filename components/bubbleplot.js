d2ed753d779dc23ecb07508d7c1e530f5adaa249
EunBeen Son <nabin111@g.skku.edu>
2024-05-30 16:04:15 +0900

    fix code

--- components/bubbleplot.js
+++ components/bubbleplot.js
@@ -10,6 +10,7 @@ class Bubbleplot {
         this.width = width;
         this.height = height;
         this.handlers = {};
+        this.currentYear = 0;
     }
 
     initialize() {
@@ -30,14 +31,23 @@ class Bubbleplot {
             .attr("height", this.height + this.margin.top + this.margin.bottom);
 
         this.container.attr("transform", `translate(${this.margin.left}, ${this.margin.top})`);
+
+        this.brush = d3.brush()
+            .extent([[0, 0], [this.width, this.height]])
+            .on("start brush", (event) => {
+                this.brushCircles(event);
+            })
     }
 
-    update(xVar, yVar, colorVar, radiusVar, currentYear) {
+    update(xVar, yVar, colorVar, radiusVar, currentYear, country) {
         this.xVar = xVar;
         this.yVar = yVar;
         this.colorVar = colorVar;
         this.radiusVar = radiusVar;
+        this.currentYear = currentYear;
+        this.country = country;
 
+        this.data.sort((a, b) => b[radiusVar] - a[radiusVar]); // sort so that smaller circles are on top
         const filteredData = this.data.filter(d => d["Year"] == currentYear);
 
         const xExtent = d3.extent(filteredData, d => d[this.xVar]);
@@ -49,7 +59,43 @@ class Bubbleplot {
 
         this.zScale.domain([...new Set(this.data.map(d => d[this.colorVar]))]).range(d3.schemeTableau10);
 
-        this.radiusScale.domain(d3.extent(this.data, d => d[this.radiusVar])).range([4, 30]);
+        this.radiusScale.domain(d3.extent(this.data, d => d[this.radiusVar])).range([6, 27]);
+
+        this.container.call(this.brush);
+
+        this.circles = this.container.selectAll("circle")
+            .data(data)
+            .join("circle")
+            .on("mouseover", (e, d) => {
+                d3.select(e.currentTarget).attr("r", parseFloat(d3.select(e.currentTarget).attr("r")) + 2).style("opacity", 1);
+
+                this.tooltip.select(".tooltip-inner")
+                    .html(`${d[this.country]}<br />${this.xVar}: ${d[this.xVar]}<br />${this.yVar}: ${d[this.yVar]}`);
+
+                Popper.createPopper(e.target, this.tooltip.node(), {
+                    placement: 'top',
+                    modifiers: [
+                        {
+                            name: 'arrow',
+                            options: {
+                                element: this.tooltip.select(".tooltip-arrow").node(),
+                            },
+                        },
+                        {
+                            name: 'offset',
+                            options: {
+                                offset: [0, 10],
+                            },
+                        },
+                    ],
+                });
+
+                this.tooltip.style("display", "block");
+            })
+            .on("mouseout", (e, d) => {
+                d3.select(e.currentTarget).attr("r", parseFloat(d3.select(e.currentTarget).attr("r")) - 2).style("opacity", 0.9);
+                this.tooltip.style("display", "none");
+            });
 
         this.circles = this.container.selectAll("circle")
             .data(filteredData)
@@ -60,6 +106,7 @@ class Bubbleplot {
             .attr("cx", d => this.xScale(d[this.xVar]))
             .attr("cy", d => this.yScale(d[this.yVar]))
             .attr("fill", d => this.zScale(d[this.colorVar]))
+            .attr("opacity", 0.9)
             .attr("r", d => this.radiusScale(d[this.radiusVar]));
 
         this.xAxis
@@ -97,5 +144,39 @@ class Bubbleplot {
             .style("font-size", ".7em")
             .attr("transform", `translate(${this.width + this.margin.left + 20}, ${this.height * 0.25})`)
             .call(d3.legendColor().scale(this.zScale))
+    }
+
+    getBrushedData(currentYear) {
+        this.currentYear = currentYear;
+        let selection = d3.brushSelection(this.container.node());
+        if (!selection) return [];
+
+        this.brushedData = this.data.filter(d => this.isBrushed(d, selection));
+        this.circles.classed("brushed", d => this.isBrushed(d, selection));
+
+        return this.brushedData;
+    }
+
+    isBrushed(d, selection) {
+        if (d["Year"] != this.currentYear) return false;
+        let [[x0, y0], [x1, y1]] = selection;
+        let x = this.xScale(d[this.xVar]);
+        let y = this.yScale(d[this.yVar]);
+
+        return x0 <= x && x <= x1 && y0 <= y && y <= y1;
+    }
+
+    brushCircles(event) {
+        let selection = event.selection;
+
+        this.brushedData = this.data.filter(d => this.isBrushed(d, selection));
+        this.circles.classed("brushed", d => this.isBrushed(d, selection));
+
+        if (this.handlers.brush)
+            this.handlers.brush(this.data.filter(d => this.isBrushed(d, selection)));
+    }
+
+    on(eventType, handler) {
+        this.handlers[eventType] = handler;
     }
 }
---
+++ components/piechart.js
@@ -0,0 +1,73 @@
+// vaccination
+class Piechart {
+    margin = {
+        top: 10, right: 20, bottom: 40, left: 40
+    }
+
+    constructor(svg, width = 200, height = 200) {
+        this.svg = d3.select(svg);
+        this.width = width;
+        this.height = height;
+        this.radius = Math.min(this.width, this.height) / 2;
+    }
+
+    initialize() {
+        this.arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(this.radius);
+
+        this.pie = d3.pie()
+            .value(d => d.value)
+            .sort(null);
+
+        this.container = this.svg.append("g")
+            .attr("transform", `translate(${this.width / 2 + this.margin.left}, ${this.height / 2 + this.margin.top})`);
+
+        this.svg
+            .attr("width", this.width + this.margin.left + this.margin.right)
+            .attr("height", this.height + this.margin.top + this.margin.bottom);
+    }
+
+    update(data, variable) {
+        const totalAvg = d3.mean(data, d => +d[variable]);
+        const vaccinatedAvg = totalAvg;
+        const notVaccinatedAvg = 100 - totalAvg;
+
+        const pieData = [
+            { label: "Vaccinated", value: vaccinatedAvg, color: "#70db70" },
+            { label: "Not Vaccinated", value: notVaccinatedAvg, color: "#ff704c" }
+        ];
+
+        const arcs = this.container.selectAll(".arc")
+            .data(this.pie(pieData));
+
+        arcs.select("path")
+            .attr("d", this.arc)
+            .attr('fill', function (d) { return d.data.color })
+            .attr("stroke", "white")
+            .attr("stroke-width", 2);
+
+        arcs.select("text")
+            .attr("transform", d => `translate(${this.arc.centroid(d)})`)
+            .attr("dy", "0.2em")
+            .style("text-anchor", "middle")
+            .text(d => `${Math.round(d.data.value * 100) / 100}%`);
+
+        const newArcs = arcs.enter().append("g")
+            .attr("class", "arc");
+
+        newArcs.append("path")
+            .attr("d", this.arc)
+            .attr('fill', function (d) { return d.data.color })
+            .attr("stroke", "white")
+            .attr("stroke-width", 2);
+
+        newArcs.append("text")
+            .attr("transform", d => `translate(${this.arc.centroid(d)})`)
+            .attr("dy", "0.2em")
+            .style("text-anchor", "middle")
+            .text(d => `${Math.round(d.data.value * 100) / 100}%`);
+
+        arcs.exit().remove();
+    }
+}
